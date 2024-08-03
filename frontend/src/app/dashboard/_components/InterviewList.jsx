db065a11c70843fe8d70febf8f9e8ac1939068a8
Yash Khamkar <yashkhamkar1100@gmail.com>
2024-08-04 00:39:55 +0530

    Update InterviewList.jsx

--- frontend/src/app/dashboard/_components/InterviewList.jsx
+++ frontend/src/app/dashboard/_components/InterviewList.jsx
@@ -52,7 +52,7 @@ function InterviewList() {
             <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-[#4845d2] mx-auto"></div>
             <h2 className="text-zinc-900 dark:text-white mt-4">Loading...</h2>
             <p class="text-zinc-600 dark:text-zinc-400">
-              Your data is loading please wait
+              Server is loading please wait
             </p>
           </div>
         </div>
