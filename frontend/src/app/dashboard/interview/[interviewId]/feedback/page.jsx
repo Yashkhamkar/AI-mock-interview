f5da92bb0a4ef3739a2e2f05ed075f0b6d7260d8
Yash <yashkhamkar1100@gmail.com>
2024-08-04 21:43:03 +0530

    Update page.jsx

--- frontend/src/app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ frontend/src/app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -51,51 +51,60 @@ function Feedback({ params }) {
 
   return (
     <div className="p-10">
-      <h2 className="text-3xl font-bold text-green-400">Congratulations</h2>
-      <h2 className="text-2xl font-bold">Here is your AI interview feedback</h2>
-      <h2 className="text-lg my-3 text-[#4845D2]">
-        Your overall interview rating:-
-        <strong>{calculateAverageRating()}/10</strong>
-      </h2>
+      {feedbackList.length > 0 ? (
+        <>
+          <h2 className="text-3xl font-bold text-green-400">Congratulations</h2>
+          <h2 className="text-2xl font-bold">
+            Here is your AI interview feedback
+          </h2>
+          <h2 className="text-lg my-3 text-[#4845D2]">
+            Your overall interview rating:-
+            <strong>{calculateAverageRating()}/10</strong>
+          </h2>
 
-      {feedbackList.length > 0 &&
-        feedbackList.map((feedback, index) => (
-          <div key={index} className="my-4">
-            <button
-              className="w-full text-left py-2 px-4 bg-gray-200 rounded-md flex justify-between items-center"
-              onClick={() => toggleFeedback(index)}
-            >
-              <span>{feedback.question}</span>
-              <span>
-                {openFeedback === index ? <ChevronUp /> : <ChevronDown />}
-              </span>
-            </button>
-            <div
-              className={`transition-max-height duration-700 ease-in-out overflow-hidden ${
-                openFeedback === index ? "max-h-screen" : "max-h-0"
-              }`}
-            >
-              <div className="flex flex-col gap-2">
-                <h2 className="text-red-500 p-2 border rounded-lg my-2">
-                  <strong>Rating: </strong>
-                  {feedback.rating}
-                </h2>
-                <h2 className="p-2 border rounded-lg bg-red-50 text-sm text-red-900">
-                  <strong>Your Answer: </strong>
-                  {feedback.userAnswer}
-                </h2>
-                <h2 className="p-2 border rounded-lg bg-green-50 text-sm text-green-900">
-                  <strong>Correct Answer Looks Like: </strong>
-                  {feedback.answer}
-                </h2>
-                <h2 className="p-2 border rounded-lg bg-blue-50 text-sm text-primary">
-                  <strong>Feedback: </strong>
-                  {feedback.feedback}
-                </h2>
+          {feedbackList.map((feedback, index) => (
+            <div key={index} className="my-4">
+              <button
+                className="w-full text-left py-2 px-4 bg-gray-200 rounded-md flex justify-between items-center"
+                onClick={() => toggleFeedback(index)}
+              >
+                <span>{feedback.question}</span>
+                <span>
+                  {openFeedback === index ? <ChevronUp /> : <ChevronDown />}
+                </span>
+              </button>
+              <div
+                className={`transition-max-height duration-700 ease-in-out overflow-hidden ${
+                  openFeedback === index ? "max-h-screen" : "max-h-0"
+                }`}
+              >
+                <div className="flex flex-col gap-2">
+                  <h2 className="text-red-500 p-2 border rounded-lg my-2">
+                    <strong>Rating: </strong>
+                    {feedback.rating}
+                  </h2>
+                  <h2 className="p-2 border rounded-lg bg-red-50 text-sm text-red-900">
+                    <strong>Your Answer: </strong>
+                    {feedback.userAnswer}
+                  </h2>
+                  <h2 className="p-2 border rounded-lg bg-green-50 text-sm text-green-900">
+                    <strong>Correct Answer Looks Like: </strong>
+                    {feedback.answer}
+                  </h2>
+                  <h2 className="p-2 border rounded-lg bg-blue-50 text-sm text-primary">
+                    <strong>Feedback: </strong>
+                    {feedback.feedback}
+                  </h2>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </>
+      ) : (
+        <h2 className="text-2xl font-bold text-red-400">
+          Please complete your interview fully to get feedback
+        </h2>
+      )}
       <Link href="/dashboard">
         <button type="button" className="cssbuttons-io-button mt-10">
           Go Home
