e36778423ff04bb5fd19ee61493635cc3afe1314
Yash <yashkhamkar1100@gmail.com>
2024-08-07 14:13:37 +0530

    Update InterviewList.jsx

--- frontend/src/app/dashboard/_components/InterviewList.jsx
+++ frontend/src/app/dashboard/_components/InterviewList.jsx
@@ -3,6 +3,7 @@ import { useUser } from "@clerk/nextjs";
 import React, { useEffect, useState } from "react";
 import InterviewItemCard from "./InterviewItemCard";
 import { toast, ToastContainer } from "react-toastify";
+
 function InterviewList() {
   const { user } = useUser();
   const [interviewList, setInterviewList] = useState([]);
@@ -28,7 +29,13 @@ function InterviewList() {
 
       const data = await response.json();
       console.log(data);
-      setInterviewList(data);
+
+      // Sort the interviews by createdAt date in descending order
+      const sortedData = data.sort(
+        (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+      );
+
+      setInterviewList(sortedData);
       setLoading(false);
     } catch (error) {
       toast.error("Error fetching interviews");
@@ -51,7 +58,7 @@ function InterviewList() {
           <div className="text-center">
             <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-[#4845d2] mx-auto"></div>
             <h2 className="text-zinc-900 dark:text-white mt-4">Loading...</h2>
-            <p class="text-zinc-600 dark:text-zinc-400">
+            <p className="text-zinc-600 dark:text-zinc-400">
               Your data is loading please wait
             </p>
           </div>
